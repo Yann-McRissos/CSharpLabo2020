b775380e63e4f3ee94e88f3e38115d2ed015a1f5
Yannick <yannick.jooris@student.hepl.be>
2020-02-10 18:18:05 +0100

    :sparkles: Added all classes (still empty though)

--- Labo_2020/CartoObj.cs
+++ Labo_2020/CartoObj.cs
@@ -2,12 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Labo_2020
 {
-	public class CartoObj
+	public abstract class CartoObj
 	{
+		private static int _id;    // must be automatically generated using a "static" object instance counter
 		
+		#region CONSTRUCTEURS
+		public CartoObj()
+		{
+			Interlocked.Increment(ref _id);
+		}
+		#endregion
+
+		#region METHODES
+		#endregion
 	}
 }
